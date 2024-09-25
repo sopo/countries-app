5c584510af4b2d6eaf436b932565bf993784b559
sopo <sofiko.imnaishvili@gmail.com>
2024-09-25 15:29:52 +0400

    file name update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./global-css/containers.css";
 import Header from "./components/header/Header";
 import CardsSection from "./components/cards/cards-section/cards-section";
 import Card from "./components/cards/card-container/card";
-import CardHeader from "./components/cards/card-header/CardHeader";
+import CardHeader from "./components/cards/card-header/card-header";
 import CardContent from "./components/cards/card-content/card-content";
 import CardFooter from "./components/cards/card-footer/card-footer";
 import Banner from "./components/banner/banner";
