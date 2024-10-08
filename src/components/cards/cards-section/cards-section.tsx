a0ed40768e79e2bfc52bf35af477c0c0e0653c04
sopo <sofiko.imnaishvili@gmail.com>
2024-10-08 12:03:46 +0400

    update cards section

--- src/components/cards/cards-section/cards-section.tsx
+++ src/components/cards/cards-section/cards-section.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from "react";
 import styles from './cards-section.module.css'
 const CardsSection: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className={`container-xl title-m margin-vertical-xl`}>
-      <p className="title-s text-primary margin-vertical-xl">New offers</p>
+  <div className={`container-xl ${styles.containerSpacing}`}>
+      <h1 className="text-primary margin-vertical-xl">Latest articles</h1>
       <div className={styles.section}>
       {children}
       </div>
