6a8bd5d96a61101fdc533e88d298822fc0cbdb81
sopo <sofiko.imnaishvili@gmail.com>
2024-10-09 15:45:35 +0400

    add sort logic

--- src/components/cards/cards-section/cards-section.tsx
+++ src/components/cards/cards-section/cards-section.tsx
@@ -1,12 +1,11 @@
 import { PropsWithChildren } from "react";
 import styles from './cards-section.module.css';
-import DropDown from "@/components/form/drop-down-menu/drop-down";
+
 const CardsSection: React.FC<PropsWithChildren> = ({ children }) => {
   return (
   <div className={`container-xl ${styles.containerSpacing}`}>
     <div className={styles.sectionTitle}>
       <h1 className="text-primary">Latest articles</h1>
-      <DropDown dropDownTitle="Sort by" />
       </div>
       <div className={styles.section}>
       {children}
--- src/pages/home/homepage.tsx
+++ src/pages/home/homepage.tsx
@@ -8,9 +8,13 @@ import CardContent from "../../components/cards/card-content/card-content";
 import CardHeader from "../../components/cards/card-header/card-header";
 import CardFooter from "../../components/cards/card-footer/card-footer";
 import RatingSection from "@/components/rating/rating-section";
+import Button from "@/components/button/button";
+
 
 const HomePage: React.FC = () => {
   const [countries, setCountries] = useState(countriesData);
+  const [sortByRating, setSortByRating] = useState(false);
+
   const handleLikeClick = (id: number) => {
     setCountries((prevCountries) =>
       prevCountries.map((country) =>
@@ -19,10 +23,20 @@ const HomePage: React.FC = () => {
     );
   };
 
+  const handleSortClick = () => {
+    const newSort = !sortByRating;
+    setSortByRating(newSort);
+    
+      setCountries((prevCountries) => 
+        [...prevCountries].sort((a, b) => newSort ? b.rating - a.rating : a.rating - b.rating)
+      );
+  };
+  const buttonSortTitle = sortByRating ? "popular last" : "popular first"
   return (
     <div>
       <Banner />
       <CardsSection>
+        <Button title={buttonSortTitle} className="buttonPrimaryM" onClick={handleSortClick}/>
         {countries.map((country) => (
           <CardContainer key={country.id}>
             <Link to={`countries/${country.id}`}>
@@ -30,15 +44,13 @@ const HomePage: React.FC = () => {
               <CardContent country={country} />
             </Link>
             <CardFooter>
-              <RatingSection
-                rating={country.rating}
-                onClick={() => handleLikeClick(country.id)}
-              />
+              <RatingSection rating={country.rating} onClick={() => handleLikeClick(country.id)} />
             </CardFooter>
           </CardContainer>
         ))}
+        
       </CardsSection>
     </div>
-  );
-};
+  )
+}
 export default HomePage;
