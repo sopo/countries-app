383c59e7f8db3702df33a6461737b4a148bc31c6
sopo <sofiko.imnaishvili@gmail.com>
2024-10-16 16:25:20 +0400

    change add article fields to controlled fields and add validation

--- src/pages/home/create-country-popup.tsx
+++ src/pages/home/create-country-popup.tsx
@@ -1,4 +1,3 @@
-
 import Popup from "@/layouts/popup/popup";
 import PopupHeader from "@/layouts/popup/popup-header/popup-header";
 import PopupBody from "@/layouts/popup/popup-body/popup-body";
@@ -6,48 +5,161 @@ import Form from "@/components/form/form";
 import Input from "@/components/form/input/input";
 import TextArea from "@/components/form/text-area/text-area";
 import Button from "@/components/button/button";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Country } from "@/components/cards/cards-data/country";
-
-interface CreateCountryPopupProps{
-    isOpen: boolean;
-    handlePopupCloseClick: () => void;
-    handleCreateArticle: (data: Country) => void;
-
+interface CreateCountryPopupProps {
+  isOpen: boolean;
+  handlePopupCloseClick: () => void;
+  handleCreateArticle: (data: Country) => void;
 }
-const CreateCountryPopup:React.FC<CreateCountryPopupProps> = ({isOpen, handlePopupCloseClick, handleCreateArticle,}) => {
- const initialCountry = {
+const CreateCountryPopup: React.FC<CreateCountryPopupProps> = ({
+  isOpen,
+  handlePopupCloseClick,
+  handleCreateArticle,
+}) => {
+
+  const initialCountry = {
     id: 0,
     name: "",
     title: "",
     population: 0,
     capital: "",
     description: "",
-    imageUrl: "https://images.unsplash.com/photo-1673179559805-8dfbf64e10d4?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl:
+      "https://images.unsplash.com/photo-1673179559805-8dfbf64e10d4?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     isDeleted: false,
     rating: 0,
- }
-  const [newCountry,setNewCountry] = useState<Country>(initialCountry);
-  function handleInputChange(key:string, value:string){
-    setNewCountry( (prev)=>({
-      ...prev,
-      [key]:value
-    }) )
+  };
+  const [newCountry, setNewCountry] = useState<Country>(initialCountry);
+  const [name, setName] = useState('')
+  const [capital, setCapital] = useState('')
+  const [title, setTitle] = useState('')
+  const [population, setPopulation] = useState('')
+  const [description, setDescription] = useState("")
+  const [nameErrorMessage, setNameErrorMessage] = useState('');
+  const [capitalErrorMessage, setCapitalErrorMessage] = useState('');
+  const [titleErrorMessage, setTitleErrorMessage] = useState('');
+  const [populationErrorMessage, setPopulationErrorMessage] = useState('');
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newName = (e.target.value)
+    setName(newName)
+    if(newName === ""){
+      setNameErrorMessage("Country name should not be empty")
+    }else{
+      setNameErrorMessage("")
+    }
+    const country = {
+      ...newCountry,
+      name: newName
+    }
+    setNewCountry(country)
   }
-  return(
-        <Popup isOpen={isOpen} > 
-        <PopupHeader title="Add new article" onClick={handlePopupCloseClick} />
-        <PopupBody>
-          <Form onSubmit={()=>handleCreateArticle(newCountry)}>
-          <Input id="name" name="country"  placeholder="Country name" onChange={(e)=> handleInputChange('name',e.target.value) }  />
-          <Input id="capital" name="capital" placeholder="Capital" onChange={(e)=> handleInputChange('capital',e.target.value) } />
-          <Input id="population" name="population" placeholder="Population" onChange={(e)=> handleInputChange('population',e.target.value) } />
-          <Input id="title" name="title" placeholder="Article title" onChange={(e)=> handleInputChange('title',e.target.value) }/>
-          <TextArea id="description" name="description" placeholder="Article description" onChange={(e)=> handleInputChange('description',e.target.value) }/>
-          <Button  title="Add article" className="buttonPrimaryM" type="submit" />
-          </Form>
-        </PopupBody>
-      </Popup>
-    )
-}
+  const handleCapitalChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newCapital = e.target.value
+    setCapital(newCapital);
+    if(newCapital === ""){
+      setCapitalErrorMessage("Capital should not be empty")
+    }else{
+      setCapitalErrorMessage("")
+    }
+    const country = {
+      ...newCountry,
+      capital: newCapital
+    }
+    setNewCountry(country)
+  }
+  const handlePopulationChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newPopulation = e.target.value;
+    const numericValue = Number(newPopulation)
+    
+    if(newPopulation !=="" && isNaN(numericValue)){
+      setPopulationErrorMessage("Population must be number")
+    }else{
+      setPopulationErrorMessage("")
+    }
+    const country = {
+      ...newCountry,
+      population: numericValue || 0
+    }
+    setPopulation(newPopulation);
+    setNewCountry(country)
+  }
+  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newTitle = e.target.value
+    setTitle(newTitle);
+    if(newTitle === ""){
+      setTitleErrorMessage("Title should not be empty")
+    }else{
+      setTitleErrorMessage("")
+    }
+    const country = {
+      ...newCountry,
+      title: newTitle
+    }
+    setNewCountry(country)
+  }
+  const handleDescriptionChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const newDescription = e.target.value
+    setDescription(newDescription);
+    const country = {
+      ...newCountry,
+      description: newDescription
+    }
+    setNewCountry(country)
+  }
+
+  return (
+    <Popup isOpen={isOpen}>
+      <PopupHeader title="Add new article" onClick={handlePopupCloseClick} />
+      <PopupBody>
+        <Form onSubmit={() => handleCreateArticle(newCountry)}>
+          <Input
+            id="name"
+            errorMessage={nameErrorMessage}
+            name="country"
+            placeholder="Country name"
+            value={name}
+            onChange={handleNameChange}
+          />
+          <Input
+            id="capital"
+            name="capital"
+            placeholder="Capital"
+            value={capital}
+            errorMessage={capitalErrorMessage}
+            onChange={handleCapitalChange}
+          />
+          <Input
+            id="population"
+            name="population"
+            placeholder="Population"
+            value={population}
+            errorMessage={populationErrorMessage}
+            onChange={handlePopulationChange}
+          />
+          <Input
+            id="title"
+            name="title"
+            placeholder="Article title"
+            errorMessage={titleErrorMessage}
+            value={title}
+            onChange={handleTitleChange}
+          />
+          <TextArea
+            id="description"
+            name="description"
+            placeholder="Article description"
+            value={description}
+            onChange={handleDescriptionChange}
+          />
+          <Button
+            title="Add article"
+            className="buttonPrimaryM"
+            type="submit"
+          />
+        </Form>
+      </PopupBody>
+    </Popup>
+  );
+};
 export default CreateCountryPopup;
