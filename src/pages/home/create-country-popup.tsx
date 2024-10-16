dc23a8044fa0f2611257b017cc8a06abf0e0e22f
sopo <sofiko.imnaishvili@gmail.com>
2024-10-16 22:51:42 +0400

    refactor create article functionality

--- src/pages/home/create-country-popup.tsx
+++ src/pages/home/create-country-popup.tsx
@@ -17,108 +17,83 @@ const CreateCountryPopup: React.FC<CreateCountryPopupProps> = ({
   handlePopupCloseClick,
   handleCreateArticle,
 }) => {
-
-  const initialCountry = {
-    id: 0,
-    name: "",
-    title: "",
-    population: 0,
-    capital: "",
-    description: "",
-    imageUrl:
-      "https://images.unsplash.com/photo-1673179559805-8dfbf64e10d4?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    isDeleted: false,
-    rating: 0,
-  };
-  const [newCountry, setNewCountry] = useState<Country>(initialCountry);
-  const [name, setName] = useState('')
-  const [capital, setCapital] = useState('')
-  const [title, setTitle] = useState('')
-  const [population, setPopulation] = useState('')
-  const [description, setDescription] = useState("")
-  const [nameErrorMessage, setNameErrorMessage] = useState('');
-  const [capitalErrorMessage, setCapitalErrorMessage] = useState('');
-  const [titleErrorMessage, setTitleErrorMessage] = useState('');
-  const [populationErrorMessage, setPopulationErrorMessage] = useState('');
-  const [descriptionErrorMessage, setDescriptionErrorMessage] = useState('')
+  const [name, setName] = useState("");
+  const [capital, setCapital] = useState("");
+  const [title, setTitle] = useState("");
+  const [population, setPopulation] = useState("");
+  const [description, setDescription] = useState("");
+  const [nameErrorMessage, setNameErrorMessage] = useState("");
+  const [capitalErrorMessage, setCapitalErrorMessage] = useState("");
+  const [titleErrorMessage, setTitleErrorMessage] = useState("");
+  const [populationErrorMessage, setPopulationErrorMessage] = useState("");
+  const [descriptionErrorMessage, setDescriptionErrorMessage] = useState("");
   const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newName = (e.target.value)
-    setName(newName)
-    if(newName === ""){
-      setNameErrorMessage("Country name should not be empty")
-    }else{
-      setNameErrorMessage("")
+    const newName = e.target.value;
+    setName(newName);
+    if (newName === "") {
+      setNameErrorMessage("Country name should not be empty");
+    } else {
+      setNameErrorMessage("");
     }
-    const country = {
-      ...newCountry,
-      name: newName
-    }
-    setNewCountry(country)
-  }
+  };
   const handleCapitalChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newCapital = e.target.value
+    const newCapital = e.target.value;
     setCapital(newCapital);
-    if(newCapital === ""){
-      setCapitalErrorMessage("Capital should not be empty")
-    }else{
-      setCapitalErrorMessage("")
+    if (newCapital === "") {
+      setCapitalErrorMessage("Capital should not be empty");
+    } else {
+      setCapitalErrorMessage("");
     }
-    const country = {
-      ...newCountry,
-      capital: newCapital
-    }
-    setNewCountry(country)
-  }
+  };
   const handlePopulationChange = (e: ChangeEvent<HTMLInputElement>) => {
     const newPopulation = e.target.value;
-    const numericValue = Number(newPopulation)
-    
-    if(newPopulation !=="" && isNaN(numericValue)){
-      setPopulationErrorMessage("Population must be number")
-    }else{
-      setPopulationErrorMessage("")
-    }
-    const country = {
-      ...newCountry,
-      population: numericValue || 0
+    const numericValue = Number(newPopulation);
+
+    if (newPopulation !== "" && isNaN(numericValue)) {
+      setPopulationErrorMessage("Population must be number");
+    } else {
+      setPopulationErrorMessage("");
     }
     setPopulation(newPopulation);
-    setNewCountry(country)
-  }
+  };
   const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newTitle = e.target.value
+    const newTitle = e.target.value;
     setTitle(newTitle);
-    if(newTitle === ""){
-      setTitleErrorMessage("Title should not be empty")
-    }else{
-      setTitleErrorMessage("")
+    if (newTitle === "") {
+      setTitleErrorMessage("Title should not be empty");
+    } else {
+      setTitleErrorMessage("");
     }
-    const country = {
-      ...newCountry,
-      title: newTitle
-    }
-    setNewCountry(country)
-  }
+  };
   const handleDescriptionChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const newDescription = e.target.value
+    const newDescription = e.target.value;
     setDescription(newDescription);
-    const country = {
-      ...newCountry,
-      description: newDescription
-    }
-    if(newDescription === ""){
-      setDescriptionErrorMessage("Description should not be empty")
-    }else{
-      setDescriptionErrorMessage("")
-    }
-    setNewCountry(country)
-  }
 
+    if (newDescription === "") {
+      setDescriptionErrorMessage("Description should not be empty");
+    } else {
+      setDescriptionErrorMessage("");
+    }
+  };
+  function createArticle() {
+    handleCreateArticle({
+      id: 0,
+      name: name,
+      capital: capital,
+      population: +population,
+      description: description,
+      title: title,
+      imageUrl:
+        "https://images.unsplash.com/photo-1673179559805-8dfbf64e10d4?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isDeleted: false,
+      rating: 0,
+    });
+  }
   return (
     <Popup isOpen={isOpen}>
       <PopupHeader title="Add new article" onClick={handlePopupCloseClick} />
       <PopupBody>
-        <Form onSubmit={() => handleCreateArticle(newCountry)}>
+        <Form onSubmit={createArticle}>
           <Input
             id="name"
             errorMessage={nameErrorMessage}
