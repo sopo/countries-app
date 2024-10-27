5777ba08ce4786dfe1538cc6a4ddca4b5614d95e
sopo <sofiko.imnaishvili@gmail.com>
2024-10-27 11:25:07 +0400

    test husky

--- src/components/tab/tab.tsx
+++ src/components/tab/tab.tsx
@@ -4,6 +4,7 @@ interface TabProps {
   onClick: () => void;
 }
 const Tab: React.FC<TabProps> = ({ tabTitle, className, onClick }) => {
+  const test=6;
   return (
     <div onClick={onClick}>
       <p className={className}>{tabTitle}</p>
