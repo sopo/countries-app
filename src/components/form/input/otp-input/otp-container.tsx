93f089c77bb6f4a9547597a9fd9c628a08458eb8
sopo <sofiko.imnaishvili@gmail.com>
2024-10-31 09:40:31 +0400

    add typescript check in pre-commit

--- src/components/form/input/otp-input/otp-container.tsx
+++ src/components/form/input/otp-input/otp-container.tsx
@@ -40,7 +40,7 @@ const OtpContainer: React.FC<NumberOfInputsProps> = ({
         return newValues;
       });
     }
-
+   
     if (newValue && /^[0-9]$/.test(newValue) && index < values.length - 1) {
       ref.current[index + 1]?.focus();
     } else if (index === values.length - 1 && newValue) {
