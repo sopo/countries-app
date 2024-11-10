d5a4a7c7df495ba3bcc2668e4d27fb8bde167928
sopo <sofiko.imnaishvili@gmail.com>
2024-11-10 18:54:49 +0400

    vercel config

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios, { CreateAxiosDefaults } from 'axios';
 const axiosConfig: CreateAxiosDefaults = {
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
 };
 const httpClient = axios.create(axiosConfig);
 export default httpClient;
